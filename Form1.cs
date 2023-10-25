d51753ab8d5df10aa1b991b7c3888a1206791cb1
Afonichkin Andrey <kmv-host@mail.ru>
2023-10-25 15:23:54 +0300

    Уже рисует, но не меняет цвета карандаша, не работают кнопки save и clear

--- Form1.cs
+++ Form1.cs
@@ -15,6 +15,7 @@ namespace Paint
         public Form1()
         {
             InitializeComponent();
+            SetSize(); // при запуске формы инициализируем метод SetSize();
         }
         private class ArrayPoints // Создаем приватный класс Массив Точек ArrayPoints
         {
@@ -47,13 +48,35 @@ namespace Paint
                 return index; //Которая возвращает индекс
             }
 
+            public Point[] GetPoints() // Метод который возвращает наш массив он нужен для отрисовки рисунка или другой графики
+            {
+                return points;
+            }
         }
+        
         //Объявляем несколько переменных
         private bool isMouse = false; // Объявили приватную переменную isMouse для проверки зажата-ли левая кнопка мыши (когда рисуем линию мышкой, рисуем зажатой кнопкой мыши)
+
+        private ArrayPoints arrayPoints = new ArrayPoints(2); // Создаем экземпляр класса ArrayPoints
+
+        Bitmap map = new Bitmap(100, 100); // Создаем переменную map для хранения изображения 100 на 100 
+
+        Graphics graphics;
+
+        Pen pen = new Pen(Color.Black, 3f); //Создаем обьект для рисования pen, инициализируем его new Pen Цвет Color черный Black толщина 3f
+
+        private void SetSize() // Устанавливаем размер для нашего Bitmap
+        {
+            Rectangle rectangle = Screen.PrimaryScreen.Bounds; //Определяем при каком разрешении работает нашь пользователь
+            map = new Bitmap(rectangle.Width, rectangle.Height); // И такое - же разрешение выставляем для Bitmap
+            graphics = Graphics.FromImage(map);
+
+        }
 
         private void pictureBox1_MouseDown(object sender, MouseEventArgs e) // Обработчик события нажатия левой кнопки мыши на pictureBox1
         {
             isMouse = true; // При нажатии левой кнопки мышки переменная isMouse приобретает значение true
+            arrayPoints.ResetPoints(); // Когда отпускаем кнопку мыши не будем ничего сохранять.
         }
 
         private void pictureBox1_MouseUp(object sender, MouseEventArgs e) // Обработчик события отпускания левой кнопки мыши на pictureBox1
@@ -63,7 +86,14 @@ namespace Paint
 
         private void pictureBox1_MouseMove(object sender, MouseEventArgs e) // Обработчик события движения мыши на pictureBox1
         {
-
+             if (!isMouse) {return; } // Если кнопка мыши не зажата if (!isMouse) то просто выходим {return;} из метода
+             arrayPoints.SetPoint(e.X, e.Y); //Задаем новую точку по координатам (e.X, e.Y)
+             if(arrayPoints.GetCountPoits() >= 2) 
+            {
+                graphics.DrawLines(pen,arrayPoints.GetPoints()); // Сделали отрисовку линии
+                pictureBox1.Image = map;
+                arrayPoints.SetPoint(e.X, e.Y);
+            }
         }
     }
 }
